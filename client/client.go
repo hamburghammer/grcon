d0ed6d814c0ce2c3c79c1bb17bd0ceab149567a2
Augusto Dwenger J <dwenger@posteo.de>
2022-03-04 21:51:29 +0100

    Add general client interface

--- client/client.go
+++ client/client.go
@@ -1,2 +1,10 @@
 // A package with client implementations to interact over RCON with different servers.
 package client
+
+// Client is the interface that should be implemented by ever client type.
+type Client interface {
+	// Auth authenticates the client against the server.
+	Auth(password string) error
+	// Exec executes a command on the remote console.
+	Exec(cmd string) ([]byte, error)
+}
